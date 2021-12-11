a350c8f30881154f07b91125e1c211344f9200c3
Danylo Owl <d.ribchinski@gmail.com>
2021-12-11 16:49:24 -0500

    Finished 1.9 and 10

---
+++ Exercises1/1_10.scm
@@ -0,0 +1,77 @@
+#lang sicp
+
+;; Ackerman's Function
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+;; What are the values of the following expressions? (Doing by hand)
+
+;; (A 1 10)
+;; (A 0 (A 1 9))
+;; (A 0 (A 0 (A 1 8)))
+;; (A 0 (A 0 (A 0 (A 1 7))))
+;; (A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+;; (A 0 (A 0 (A 0 (A 0 64))))
+;; (A 0 (A 0 (A 0 128)))
+;; (A 0 (A 0 256))
+;; (A 0 512)
+;; 1,024
+(A 1 10)
+
+;; (A 2 4)
+;; (A 1 (A 2 3))
+;; (A 1 (A 1 (A 2 2)))
+;; (A 1 (A 1 (A 1 (A 2 1))))
+;; (A 1 (A 1 (A 1 2)))
+;; (A 1 (A 1 (A 0 (A 1 1))))
+;; (A 1 (A 1 (A 0 2)))
+;; (A 1 (A 1 4))
+;; (A 1 (A 0 (A 1 3)))
+;; (A 1 (A 0 (A 0 (A 1 2))))
+;; (A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+;; (A 1 (A 0 (A 0 (A 0 2))))
+;; (A 1 (A 0 (A 0 4)))
+;; (A 1 (A 0 8))
+;; (A 1 16)
+;; ... I am starting to see a pattern here.
+;; if x = 1, then A = 2^y
+;; therefore 2^16 = 65,536
+(A 2 4)
+
+;; (A 3 3)
+;; (A 2 (A 3 2))
+;; (A 2 (A 2 (A 3 1))
+;; (A 2 (A 2 2))
+;; (A 2 (A 1 (A 2 1)))
+;; (A 2 (A 1 2))
+;; Knowing the if x = 1, then 2^y
+;; (A 2 4)
+;; Knowing the result of the previous statement
+;; 65,536
+(A 3 3)
+
+;; Give concise mathematical definitions for the functions com-
+;; puted by the procedures f, g, and h for positive integer val-
+;; ues of n.
+
+;; 2n
+(define (f n) (A 0 n))
+;; n = 0, 0; otherwise 2^n
+(define (g n) (A 1 n))
+;; n = 0, 0; n = 1, 2; n > 1, 2^(2^...(n many times))
+(define (h n) (A 2 n))
+;; Given: 5n^2
+(define (k n) (* 5 n n))
